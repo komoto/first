c3c894207d854208472734babd41b1b3a4d5e298
komoto <hiroshi.komoto@nex.nikkei.co.jp>
2015-08-08 17:23:54 +0900

    practice css

--- public-of-visualdata/prj/averageincome/js/salary.js
+++
@@ -1,3 +0,0 @@
-scatter_salary.init.call(scatter_salary);
-
-$("body").attr("id","tpl-eployee_immersive-index").attr("class","tpl-eployee_immersive");
--- public-of-visualdata/prj2/sharpreport-2015/js/custom.js
+++
@@ -1,9 +0,0 @@
-	// // bootstrap-slider.js
-	// $("#slider01").slider({
-	// 	tooltip: "always",
-	// 	formatter: function(value) {
-	// 		// this.year = value;
-	// 		$("#year").text(value);
-	// 		return value + "年度";
-	// 	}
-	// });
